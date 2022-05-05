569bb5a46633daa5cef0b1186563d2881acaedf1
sayoriaaa <sayoriaaa@outlook.com>
2022-05-05 10:16:37 +0800

    repeated work finished

--- parser.cpp
+++ parser.cpp
@@ -101,30 +101,168 @@ int parser::ifsent(){
     std::list<struct Token*>::iterator scope_iter=token_iter;
     if(match(K_IF)&&go()&&condition()&&go()&&match(K_THEN)&&go()&&sentence()) return 1;
     token_iter=scope_iter;
-    retrun 0;
+    return 0;
 }
 int parser::whilesent(){
     std::list<struct Token*>::iterator scope_iter=token_iter;
     if(match(K_WHILE)&&go()&&condition()&&go()&&match(K_DO)&&go()&&sentence()) return 1;
     token_iter=scope_iter;
-    retrun 0;
+    return 0;
 }
 int parser::write(){
-
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_WRITE)&&go()&&match(L_PAREN)&&go()&&express()&&go()&&exprsuff()&&go()&&match(R_PAREN)) return 1;
+    token_iter=scope_iter;
+    return 0;
 }
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
-void parser::
+int parser::exprsuff(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(O_COMMA)&&go()&&express()&&go()&&exprsuff()) return 1;
+    token_iter=scope_iter;
+    return 1; 
+}
+int parser::condition(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(express()&&go()&&relationopt()&&go()&&express()) return 1;
+    token_iter=scope_iter;
+    if(match(K_ODD)&&go()&&express()) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::express(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(O_ADD)&&go()&&term()&&go()&&termsuff()) return 1;
+    token_iter=scope_iter;
+    if(match(O_MUS)&&go()&&term()&&go()&&termsuff()) return 1;
+    token_iter=scope_iter;
+    if(term()&&go()&&termsuff()) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::factorsuff(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(mulopt()&&go()&&factor()&&go()&&factorsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::factor(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(IDENTIFIER)) return 1;
+    token_iter=scope_iter;
+    if(match(INT)) return 1;
+    token_iter=scope_iter;
+    if(match(L_PAREN)&&go()&&express()&&go()&&match(R_PAREN)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::mulopt(){
+    if(match(O_MUL)||match(O_DIV)) return 1;
+    return 0;
+}
+
+
+int parser::programhead(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_PROGRAM)&&go()&&match(IDENTIFIER)&&go()&&match(SEMICOLON)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::constexp(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_CONST)&&go()&&constdef()&&go()&&constsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::varexp(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_VAR)&&go()&&vardef()&&go()&&varsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::vardef(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(IDENTIFIER)&&go()&&idsuff()&&go()&&match(COLON)&&go()&&type()&&go()&&match(SEMICOLON)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::prodef(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(prohead()&&go()&&block()&&go()&&match(SEMICOLON)&&go()&&prodef()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::prohead(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_PROCEDURE)&&go()&&argument()&&go()&&match(SEMICOLON)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::assign_or_call(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(IDENTIFIER)&&go()&&suffix()) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::suffix(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(ASSIGN)&&go()&&express()) return 1;
+    token_iter=scope_iter;
+    if(match(L_PAREN)&&go()&&express()&&go()&&match(R_PAREN)) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::read(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_READ)&&go()&&match(L_PAREN)&&go()&&match(IDENTIFIER)&&go()&&idsuff()&&go()&&match(R_PAREN)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::idsuff(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(O_COMMA)&&go()&&match(IDENTIFIER)&&go()&&idsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::compsent(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(K_BEGIN)&&go()&&sentence()&&go()&&sentsuff()&&go()&&match(K_END)) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::sentsuff(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(SEMICOLON)&&go()&&sentence()&&go()&&sentsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::termsuff(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(addopt()&&go()&&term()&&go()&&termsuff()) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::term(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(factor()&&go()&&factorsuff()) return 1;
+    token_iter=scope_iter;
+    return 0;
+}
+int parser::argument(){
+    std::list<struct Token*>::iterator scope_iter=token_iter;
+    if(match(L_PAREN)&&go()&&match(IDENTIFIER)&&go()&&match(COLON)&&go()&&type()&&go()&&match(R_PAREN)) return 1;
+    token_iter=scope_iter;
+    return 1;
+}
+int parser::addopt(){
+    if(match(O_ADD)||match(O_MUS)) return 1;
+    return 0;
+}
+int parser::relationopt(){
+    if(match(O_EQUAL, O_GREATER_EQUAL)) return 1;
+    return 0;
+}
+
+
 
 int main(){
     struct parser *myparser = new struct parser;
